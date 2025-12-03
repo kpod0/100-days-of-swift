2fc900e7d36cf4688ccee4f2cf439a01ef404f50
Konrad (Rivss) Podrygalski <konrad.podrygalski@gmail.com>
2025-12-03 19:24:06 +0100

    Initialized 100 Days of Swift project

---
+++ Playgrounds/Playground01.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+let firstName = "Konrad"
+let lastName = "Podrygalski"
+var age = 29
+
+print("Hello, I am \(firstName) \(lastName). My age is \(age).")
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# 100 Days of Swift
+
+[Day 01](https://github.com/kpod0/100-days-of-swift/Playgrounds/)
